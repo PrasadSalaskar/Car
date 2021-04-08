b6693f5b9f254760f67c4ff3b85366bb8e3690d0
sysadmin <sysadmin@bcalab1-m05>
2021-04-08 08:45:05 +0530

    second commit

--- src/Car.java
+++ src/Car.java
@@ -8,6 +8,11 @@ public class Car {
 	this.brand = brand; 
 	this.speed = speed; 
 	} 
+	void change_speed(int speed) 
+	{ 
+	this.speed = speed; 
+	} 
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
